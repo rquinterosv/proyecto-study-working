3b4087ad4bbf16b1764657f20ce3a46286730a5d
rquinterosv <rafael.quinterosv@gmail.com>
2023-11-21 13:01:17 +1300

    Actualizando menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,12 +3,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+
 function Menu() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home">SW Experience</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
