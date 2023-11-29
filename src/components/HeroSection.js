88adc1a42e24f1404b6bfe48c8f9657d25e3524e
rquinterosv <rafael.quinterosv@gmail.com>
2023-11-29 14:41:37 +1300

    Ajustando imagen de hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import heroimg from '../img/Hero.png';
 
 export default function HeroSection() {
   return (
-    <header style={{ paddingLeft: 0, backgroundImage: "url('https://mdbootstrap.com/img/new/slides/041.webp')", height: 400, backgroundSize: 'cover', width: '100%', }}>
+    <header
+      style={{
+        paddingLeft: 0,
+        backgroundImage: `url(${heroimg})`,
+        height: 400,
+        minHeight: 200,
+        backgroundSize: 'cover', // Adjust the value here
+        backgroundRepeat: 'no-repeat', // Ensure the image doesn't repeat
+        backgroundPosition: 'center', // Center the image
+        width: '100%',
+      }}
+    >
       <div className='p-5 text-center'>
-        <div className='mask p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}>
+        <div className='mask p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
           <div className='d-flex justify-content-center align-items-center h-100'>
             <div className='text-white'>
               <h1 className='mb-3'>Estudia y trabaja en el extranjero</h1>
