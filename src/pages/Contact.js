90ba909746b85268e2c975a121a5243ba1f99e81
rquinterosv <rafael.quinterosv@gmail.com>
2023-12-02 11:40:32 +1300

    Agregando contact page information

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,113 @@
-// Contact.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../css/contact.css'; // Si necesitas estilos adicionales
 
 const Contact = () => {
-  return <div>Contact Component</div>;
+
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        if (!response.ok) {
+          throw new Error('Failed to fetch countries');
+        }
+        const data = await response.json();
+        const countriesList = data.map((country) => country.name.common);
+        setCountries(countriesList);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
+  const countryOptions = countries.map((country, index) => (
+    <option key={index} value={country}>
+      {country}
+    </option>
+  ));
+
+
+  return (
+    <div className="container">
+      <div className="row">
+      <div className="col-md-6 p-4">
+          {/* Información de contacto y formulario */}
+          <div className="contact-form">
+            <h2>Formulario de Contacto</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="nombre">Nombre:</label>
+                <input type="text" id="nombre" name="nombre" className="form-control" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="apellido">Apellido:</label>
+                <input type="text" id="apellido" name="apellido" className="form-control" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="correo">Correo:</label>
+                <input type="email" id="correo" name="correo" className="form-control" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="nacionalidad">Nacionalidad:</label>
+                {isLoading ? (
+                  <p>Cargando países...</p>
+                ) : (
+                  <select id="nacionalidad" name="nacionalidad" className="form-control">
+                    <option value="">Selecciona un país</option>
+                    {countryOptions}
+                  </select>
+                )}
+              </div>
+              <div className="form-group">
+                <label htmlFor="opciones">Opciones (Elige una):</label>
+                <select id="opciones" name="opciones" className="form-control">
+                  <option value="opcion1">Opción 1</option>
+                  <option value="opcion2">Opción 2</option>
+                  <option value="opcion3">Opción 3</option>
+                  <option value="opcion4">Opción 4</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="secciones">Secciones (Elige una):</label>
+                <select id="secciones" name="secciones" className="form-control">
+                  <option value="seccion1">Sección 1</option>
+                  <option value="seccion2">Sección 2</option>
+                  <option value="seccion3">Sección 3</option>
+                  <option value="seccion4">Sección 4</option>
+                </select>
+              </div>
+              <button type="submit" className="btn btn-primary">Enviar</button>
+            </form>
+          </div>
+        </div>
+        <div className="col-md-6 p-4">
+          {/* Información del profesional */}
+          <div className="contact-info">
+            <h2>Información de Contacto</h2>
+            <p>Dirección: [Dirección]</p>
+            <p>Teléfono: [Teléfono]</p>
+            <p>Email: [Email]</p>
+            {/* Puedes agregar más información de contacto según sea necesario */}
+          </div>
+          <div className="professional-profile text-center">
+            <img
+              src="URL_DE_LA_IMAGEN"
+              alt="Foto del profesional"
+              className="profile-picture rounded-circle img-fluid"
+            />
+            <h3>Nombre Apellido</h3>
+            <p>Cargo</p>
+            <p>Descripción breve del profesional.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
